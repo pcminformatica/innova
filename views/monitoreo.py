ea7d975fdd86e00e995f5b74e222de4b38030955
Pedro Varela <consultorvarela@gmail.com>
2023-06-29 18:28:14 -0600

    lista de empresarias actuliza fix no id

--- views/monitoreo.py
+++ views/monitoreo.py
@@ -705,7 +705,6 @@ def _diagnosis_dashboard(user_uid):
     company =  Company.query.filter(Company.id == user_uid).first()
     carta = CatalogIDDocumentTypes.query.filter_by(name_short='DOC2').first()
     carta = DocumentCompany.query.filter_by(company_id=current_user.extra_info.company.id,documente_type_id=carta.id,enabled=True).first()
-    company = Company.query.filter_by(id=current_user.extra_info.company_id).first()
     diagnos = DiagnosisCompany.query.filter_by(company_id=company.id,status=True).order_by(asc(DiagnosisCompany.date_created)).first()
     #actions = ActionPlan.query.join(CatalogServices, ActionPlan.services_id==CatalogServices.id).filter(ActionPlan.company_id==company.id,ActionPlan.fase!=0,ActionPlan.cancelled ==False).order_by(asc(ActionPlan.date_scheduled_start)).all()
     actions = ActionPlan.query.join(CatalogServices, ActionPlan.services_id==CatalogServices.id).filter(ActionPlan.company_id==company.id,ActionPlan.fase!=0,ActionPlan.cancelled ==False).order_by(asc(ActionPlan.date_scheduled_start)).all()
