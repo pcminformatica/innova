06a0a9b7725f1aab8a4f963bcbeedcb2493cbb34
Pedro Varela <consultorvarela@gmail.com>
2023-09-18 09:39:01 -0600

    actulizacion de innova companys reporte

--- views/monitoreo.py
+++ views/monitoreo.py
@@ -645,7 +645,16 @@ def _indicadores_perfil_asesor(user_uid):
             diagnosticos = list(e for e in api['results'] if e['_submitted_by']  in user.extra_info.kobotoolbox['kobotoolbox_access'] and range1 <= convertir_a_datetime(e['_submission_time']) <= range2  )
             print(diagnosticos)
             print(len(diagnosticos))
-        companys = Company.query.join(User, User.id==Company.created_by).join(CompanyStatus, Company.status_id==CompanyStatus.id).filter(Company.date_created.between(txt_start_date, txt_end_date), Company.enabled==True, Company.created_by == user.id,CompanyStatus.name_short !=1 ).all()
+        #companys = Company.query.join(User, User.id==Company.created_by).join(CompanyStatus, Company.status_id==CompanyStatus.id).filter(Company.date_created.between(txt_start_date, txt_end_date), Company.enabled==True, Company.created_by == user.id,CompanyStatus.name_short !=1 ).all()
+        companys = Company.query.join(User, User.id == Company.created_by).join(CompanyStatus, Company.status_id == CompanyStatus.id).filter(
+            or_(
+                Company.date_created.between(txt_start_date, txt_end_date),
+                Company.date_action_plan.between(txt_start_date, txt_end_date)
+            ),
+            Company.enabled == True,
+            Company.created_by == user.id,
+            CompanyStatus.name_short != 1
+        ).all()
         companys_etapa1 = Company.query.join(User, User.id==Company.created_by
                                             ).join(CompanyStatus, Company.status_id==CompanyStatus.id
                                             ).join(CompanyStage, Company.stage_id==CompanyStage.id
